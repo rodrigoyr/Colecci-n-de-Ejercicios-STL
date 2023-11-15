f853ff6f16c0aeaa851fc2ff5239a3341fde5044
rodrigoyr <ryeperub@myuax.com>
2023-11-15 18:58:16 +0100

    trying code commit

--- Ejercicio 2/main.cpp
+++ Ejercicio 2/main.cpp
@@ -1,2 +1,9 @@
 #include <iostream>
+#include "triangulo.cpp"
+#include "circulo.cpp"
 
+int main() {
+    std::cout << "Area del triangulo: " << Geometria::calcularAreaTriangulo(5, 3) << std::endl;
+    std::cout << "Area del circulo: " << Geometria::calcularAreaCirculo(3) << std::endl;
+    return 0;
+}
