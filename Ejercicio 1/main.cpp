37eb1fc35debe56e6cd1c38da7e755851abf49fc
rodrigoyr <ryeperub@myuax.com>
2023-11-15 18:57:01 +0100

    correct code solution commit

--- Ejercicio 1/main.cpp
+++ Ejercicio 1/main.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <string>
+
 namespace Matematicas {
     int suma(int a, int b) {
         return a + b;
@@ -11,3 +14,4 @@ int main() {
     std::cout << "Suma: " << Matematicas::suma(5, 3) << std::endl;
     std::cout << "Resta: " << Matematicas::resta(5, 3) << std::endl;
     return 0;
+}
