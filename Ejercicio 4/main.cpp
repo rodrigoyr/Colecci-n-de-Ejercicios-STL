db4bd9c88b2c01930b47e16dc2f0a903529bd409
rodrigoyr <ryeperub@myuax.com>
2023-11-15 19:08:22 +0100

    code commit

--- Ejercicio 4/main.cpp
+++ Ejercicio 4/main.cpp
@@ -1,2 +1,15 @@
 #include <iostream>
 
+namespace Ciencia {
+    namespace Fisica {
+        const double c = 299792458;
+        double calcularEnergia(double masa) {
+            return masa * c * c;
+        }
+    }
+}
+
+int main() {
+    std::cout << "Energia: " << Ciencia::Fisica::calcularEnergia(10) << std::endl;
+    return 0;
+}
